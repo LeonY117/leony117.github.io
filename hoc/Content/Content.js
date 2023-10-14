bb623b11d8ff4dda86a499687bd91b886a68220a
Leon Yao <leon.yao117@gmail.com>
2023-10-14 15:05:44 +0100

    create content component

---
+++ hoc/Content/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './Content.module.css';
+
+const Content = (props) => {
+  let classArray = [classes.Content];
+  return <div className={classArray.join(' ')}>{props.children}</div>;
+};
+
+export default Content;
