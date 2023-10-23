93aeff6a3e3b35651f8cdb77e4cbfc573b528d07
Leon Yao <leon.yao117@gmail.com>
2023-10-23 16:01:54 +0100

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+
+import { Analytics } from '@vercel/analytics/react';
 config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps: { ...pageProps } }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
